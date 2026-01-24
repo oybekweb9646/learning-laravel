aba811eec3c9c5d048745c8994445fd724c8497b
Sardor <86460928+Sardor-21@users.noreply.github.com>
2026-01-24 15:07:33 +0500

    made changes

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,7 @@ return [
     */
     'paths'                    => ['api/*'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['*'],
+    'allowed_origins'          => ['*', 'http://localhost:5173'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
